cca002f6f5e15a3afd772a690569dd8d985ec531
shubham <shubh.tiwari512@gmail.com>
2020-10-05 20:16:00 +0545

    docstrings added for SCOREs

--- score/lendingPoolConfigurator/lendingPoolConfigurator.py
+++ score/lendingPoolConfigurator/lendingPoolConfigurator.py
@@ -15,52 +15,141 @@ class LendingPoolConfigurator(IconScoreBase):
         super().on_update()
 
     @external
-    def initReserve(_reserve:Address,_underlyingAssetDecimals:int):
+    def initReserve(self, _reserve: Address, _underlyingAssetDecimals: int):
+        """
+        initializes a reserve
+        :param _reserve:the address of reserve to be initialized
+        :param _underlyingAssetDecimals:the decimals of the reserve underlying asset
+        :return:
+        """
 
     @external
-    def initReserveWithData(_reserve:Address,_underlyingAssetDecimals:int,_oTokenName:str,_oTokenSymbol:str):
+    def initReserveWithData(self, _reserve: Address, _underlyingAssetDecimals: int, _oTokenName: str,
+                            _oTokenSymbol: str):
         """
         initializes a reserve using aTokenData provided externally (useful if the underlying ERC20 contract doesn't expose name or decimals)
-    
-        """   
-    @external    
-    def removeLastAddedReserve(_reserveToRemove:Address):
+        :param _reserve: the address of reserve to be initialized
+        :param _underlyingAssetDecimals: the decimals of the reserve underlying asset
+        :param _oTokenName: the name of  oToken
+        :param _oTokenSymbol:the symbol of oToken
+        :return:
+        """
 
     @external
-    def enableBorrowingOnReserve(_reserve:Address):
+    def removeLastAddedReserve(self, _reserveToRemove: Address):
+        """
+        removes the last added reserve
+        :param _reserveToRemove: the address of the reserve to remove
+        :return:
+        """
 
     @external
-    def disableBorrowingOnReserve(_reserve:Address):
+    def enableBorrowingOnReserve(self, _reserve: Address):
+        """
+        enables the borrow from _reserve
+        :param _reserve:address of the reserve
+        :return:
+        """
 
     @external
-    def enableReserveAsCollateral(_reserve:Address,_baseLTVasCollateral:int,_liquidationThreshold:int,_liquidationBonus:int):
-    
-    @external
-    def disableReserveAsCollateral(_reserve:Address):
+    def disableBorrowingOnReserve(self, _reserve: Address):
+        """
+        disables the borrow from _reserve
+        :param _reserve:address of the reserve
+        :return:
+        """
 
     @external
-    def activateReserve(__reserve:Address):
-    
-    @external
-    def deactiveReserve(_reserve:Address):
-    
-    @external
-    def freezeReserve(_reserve:Address):
+    def enableReserveAsCollateral(self, _reserve: Address, _baseLTVasCollateral: int, _liquidationThreshold: int,
+                                  _liquidationBonus: int):
+        """
+        enables borrow from a reserve
+        :param _reserve:address of the reserve
+        :param _baseLTVasCollateral:the loan to value of asset when used as collateral
+        :param _liquidationThreshold:the threshold at which loans using this asset as collateral will be considered undercollateralized
+        :param _liquidationBonus:the bonus liquidators receive to liquidate the asset
+        :return:
+        """
 
     @external
-    def unfreezeReserve(_reserve:Address):
-    
-    @external
-    def setReserveBaseLTVasCollateral(_reserve:Address,_ltv:int):
+    def disableReserveAsCollateral(self, _reserve: Address):
+        """
+        disables borrow from a reserve
+        :param _reserve: address of the reserve
+        :return:
+        """
 
     @external
-    def setReserveLiquidationThreshold(_reserve:Address,_threshold:int):
-    
+    def activateReserve(self, _reserve: Address):
+        """
+        activates a reserve
+        :param _reserve:address of the reserve
+        :return:
+        """
+
     @external
-    def setReserveLiquidationBonus(_reserve:Address,_bonus:int):
-    
+    def deactiveReserve(self, _reserve: Address):
+        """
+        deactivates a reserve
+        :param _reserve:address of the reserve
+        :return:
+        """
+
     @external
-    def setReserveDecimals(_reserve:Address,_decimals:int):
-    
+    def freezeReserve(self, _reserve: Address):
+        """
+        freezes a reserve
+        :param _reserve: address of the reserve
+        :return:
+        """
+
     @external
-    def refreshLendingPoolCoreConfiguration():
+    def unfreezeReserve(self, _reserve: Address):
+        """
+        unfreezes a reserve
+        :param _reserve: address of the reserve
+        :return:
+        """
+
+    @external
+    def setReserveBaseLTVasCollateral(self, _reserve: Address, _ltv: int):
+        """
+        sets/updates  the loan to value of a reserve
+        :param _reserve:address of the reserve
+        :param _ltv:the value of ltv to set
+        :return:
+        """
+
+    @external
+    def setReserveLiquidationThreshold(self, _reserve: Address, _threshold: int):
+        """
+        updates the liquidation threshold of the reserve
+        :param _reserve:address of the reserve
+        :param _threshold:the value liquidation threshold to set
+        :return:
+        """
+
+    @external
+    def setReserveLiquidationBonus(self, _reserve: Address, _bonus: int):
+        """
+        updates the liquidation bonus of the reserve
+        :param _reserve: address of the reserve
+        :param _bonus: the value for liquidation bonus
+        :return:
+        """
+
+    @external
+    def setReserveDecimals(self, _reserve: Address, _decimals: int):
+        """
+        updates the reserve decimals
+        :param _reserve:address of the reserve
+        :param _decimals:the new number of  decimals
+        :return:
+        """
+
+    @external
+    def refreshLendingPoolCoreConfiguration(self):
+        """
+        refreshes the lending pool core configuration to update the cached address
+        :return:
+        """
--- score/lendingPoolCore/lendingPoolCore.py
+++ score/lendingPoolCore/lendingPoolCore.py
@@ -13,87 +13,261 @@ class LendingPoolCore(IconScoreBase):
 
     def on_update(self) -> None:
         super().on_update()
-    
-    @external
-    def updateStateOnDeposit(_reserve:Address,_user:Address,_amount:int,_isFirstDeposit:bool):
 
     @external
-    def updateStateOnRedeem(_reserve:Address,_user:Address,_amountRedeemed:int,_userRedeemedEverything:bool):
+    def updateStateOnDeposit(self, _reserve: Address, _user: Address, _amount: int, _isFirstDeposit: bool):
+        """
+        updates the states to handle a deposit action by the _user
+        :param _reserve:address of the reserve
+        :param _user:address of the user
+        :param _amount:amount to be deposited
+        :param _isFirstDeposit:true if the user don't have any previous deposit
+        :return:
+        """
 
     @external
-    def updateStateOnBorrow(_reserve:Address,_user:Address,_amountBorrowed:int,_borrowFee:int):
+    def updateStateOnRedeem(self, _reserve: Address, _user: Address, _amountRedeemed: int,
+                            _userRedeemedEverything: bool):
+        """
+        updates the states to handle a redeem action by the _user
+        :param _reserve: address of the reserve
+        :param _user: address of the user
+        :param _amountRedeemed: amount to be redeemed
+        :param _userRedeemedEverything: true if the user is redeeming everything
+        :return:
+        """
 
     @external
-    def updateStateOnRepay(_reserve:Address,_user:Address,_paybackAmountMinusFees,_originationFeeRepaid:int,_balanceIncrease:int,_repaidWholeLoan:bool):
+    def updateStateOnBorrow(self, _reserve: Address, _user: Address, _amountBorrowed: int, _borrowFee: int):
+        """
+        updates the states to handle a borrow action by the _user
+        :param _reserve:address of the reserve
+        :param _user: address of the user
+        :param _amountBorrowed: amount to be borrowed
+        :param _borrowFee: fee to be paid for borrow amount
+        :return:
+        """
 
     @external
-    def updateStateOnLiquidation(_principalReserve:Address,_collateralReserve:Address,_user:Address,_amountToLiquidate:int,_collateralToLiquidate:int,_feeLiquidated:int,_liquidatedCollateralForFee:int,_balanceIncrease:int):
+    def updateStateOnRepay(self, _reserve: Address, _user: Address, _paybackAmountMinusFees, _originationFeeRepaid: int,
+                           _balanceIncrease: int, _repaidWholeLoan: bool):
+        """
+        updates the states to handle a loan repay action by the _user
+        :param _reserve: address of the reserve
+        :param _user: address of the user
+        :param _paybackAmountMinusFees: amount to be paid back after the deduction of the borrow fee
+        :param _originationFeeRepaid: amount of fee to be paid
+        :param _balanceIncrease:accrued interest on borrow amount
+        :param _repaidWholeLoan:true in case the user is clearing all the loan
+        :return:
+        """
 
     @external
-    def transferToUser(_reserve:Address,_user:Address,_amount:int):
+    def updateStateOnLiquidation(self, _principalReserve: Address, _collateralReserve: Address, _user: Address,
+                                 _amountToLiquidate: int, _collateralToLiquidate: int, _feeLiquidated: int,
+                                 _liquidatedCollateralForFee: int, _balanceIncrease: int):
+        """
+        updates the state of collateral and principal reserve to handle the liquidation call by a liquidator
+        :param _principalReserve:address of the principal reserve that is being paid
+        :param _collateralReserve:address of the collateral that is being liquidated
+        :param _user:address of the user
+        :param _amountToLiquidate:amount paid by the liquidator
+        :param _collateralToLiquidate:amount of collateral being liquidated
+        :param _feeLiquidated:amount of origination fee liquidated
+        :param _liquidatedCollateralForFee:the amount of collateral equivalent to the origination fee + bonus
+        :param _balanceIncrease:accrues interest on the borrowed amount
+        :return:
+        """
 
     @external
-    def transferToFeeCollectionAddress(_token:Address,_user:Address,_amount:int,_destination:Address):
+    def transferToUser(self, _reserve: Address, _user: Address, _amount: int):
+        """
+        transfers asset from reserve to the user
+        :param _reserve:address of the reserve
+        :param _user:address of the user
+        :param _amount:amount to be transferred
+        :return:
+        """
 
     @external
-    def liquidateFee(_token:Address,_amount:int,_destination:Address):
+    def transferToFeeCollectionAddress(self, _token: Address, _user: Address, _amount: int, _destination: Address):
+        """
+        transfer the fee to fee collection address
+        :param _token:address of token being transferred
+        :param _user:address of the user from where the transfer is performed
+        :param _amount:amount to be transferred
+        :param _destination:fee collection address
+        :return:
+        """
 
     @external
-    def transferToReserve(_reserve:Address,_user:Address,_amount:int):
+    def liquidateFee(self, _token: Address, _amount: int, _destination: Address):
+        """
+        transfers the fees to the fees collection address in the case of liquidation
+        :param _token:address of token being transferred
+        :param _amount:amount to be transferred
+        :param _destination:fee collection address
+        :return:
+        """
 
     @external
-    def setReserveBaseLTVasCollateral(_reserve:Address,_ltv:int):
+    def transferToReserve(self, _reserve: Address, _user: Address, _amount: int):
+        """
+        transfers an asset to the reserve
+        :param _reserve:address of the reserve
+        :param _user:address of the user
+        :param _amount:amount to be transferred
+        :return:
+        """
 
     @external
-    def setReserveLiquidationThreshold(_reserve:Address,_threshold:int):
-    
-    @external
-    def setReserveLiquidationBonus(_reserve:Address,_bonus:int):
-    
-    @external
-    def setReserveDecimals(_reserve:Address,_decimals:int):
-    
-    @external
-    def refreshConfiguration():
-    
-    @external
-    def initReserve(_reserve:Address,_oTokenAddress:Address,_decimals:int)
-
-    @external 
-    def removeLastAddedReserve(_reserveToRemove:Address):
-    
-    @external
-    def activateReserve(_reserve:Address):
-    
-    @external
-    def deactivateReserve(_reserve:Address):
+    def setReserveBaseLTVasCollateral(self, _reserve: Address, _ltv: int):
+        """
+        sets/updates  the loan to value of a reserve
+        :param _reserve:address of the reserve
+        :param _ltv:the value of ltv to set
+        :return:
+        """
 
     @external
-    def freezeReserve(_reserve:Address):
+    def setReserveLiquidationThreshold(self, _reserve: Address, _threshold: int):
+        """
+        updates the liquidation threshold of the reserve
+        :param _reserve:address of the reserve
+        :param _threshold:the value liquidation threshold to set
+        :return:
+        """
 
     @external
-    def unfreezeReserve(_reserve:Address):
-    
+    def setReserveLiquidationBonus(self, _reserve: Address, _bonus: int):
+        """
+        updates the liquidation bonus of the reserve
+        :param _reserve: address of the reserve
+        :param _bonus: the value for liquidation bonus
+        :return:
+        """
+
+    @external
+    def setReserveDecimals(self, _reserve: Address, _decimals: int):
+        """
+        updates the reserve decimals
+        :param _reserve:address of the reserve
+        :param _decimals:the new number of  decimals
+        :return:
+        """
+
+    @external
+    def refreshConfiguration(self):
+        """
+        updates the lending pool core configuration
+        :return:
+        """
+
+    @external
+    def initReserve(self, _reserve: Address, _oTokenAddress: Address, _decimals: int):
+        """
+        initilizes a reserve
+        :param _reserve: address of the reserve
+        :param _oTokenAddress: address of the oToken contract
+        :param _decimals: decimals of the reserve 
+        :return: 
+        """
+
+    @external
+    def removeLastAddedReserve(self, _reserveToRemove: Address):
+        """
+        removes the last added reserve
+        :param _reserveToRemove: the address of the reserve to remove
+        :return:
+        """
+
+    @external
+    def activateReserve(self, _reserve: Address):
+        """
+        activates a reserve
+        :param _reserve:address of the reserve
+        :return:
+        """
+
+    @external
+    def deactivateReserve(self, _reserve: Address):
+        """
+        deactivates a reserve
+        :param _reserve:address of the reserve
+        :return:
+        """
+
+    @external
+    def freezeReserve(self, _reserve: Address):
+        """
+        freezes a reserve
+        :param _reserve: address of the reserve
+        :return:
+        """
+
+    @external
+    def unfreezeReserve(self, _reserve: Address):
+        """
+        unfreezes a reserve
+        :param _reserve: address of the reserve
+        :return:
+        """
+
     @external(readonly=True)
-    def getUserBasicReserveData(_reserve:Address,_user:Address)->dict:
+    def getUserBasicReserveData(self, _reserve: Address, _user: Address) -> dict:
+        """
+        returns the basic data (balances, fee accrued, reserve enabled/disabled as collateral),
+        needed to calculate the global account data in the LendingPoolDataProvider
+        :param _reserve:address of the reserve
+        :param _user:address of the user
+        :return:the user deposited balance, the principal borrow balance, the fee, and if the reserve is enabled as collateral or not in the form of a dict
+        """
 
     @external(readonly=True)
-    def getUserUnderlyingAssetBalance(_reserve:Address,_user:Address)->int:
-    
-    @external(readonly=True)
-    def getReserveOTokenAddress(_reserve:Address)->Address:
+    def getUserUnderlyingAssetBalance(self, _reserve: Address, _user: Address) -> int:
+        """
+        gets the underlying asset balance of a user based on the corresponding oToken balance.
+        :param _reserve:address of the reserve
+        :param _user:address of the user
+        :return:the underlying deposit balance of the user
+        """
 
     @external(readonly=True)
-    def getReserveAvailableLiquidity(_reserve:Address)->int:
+    def getReserveOTokenAddress(self, _reserve: Address) -> Address:
+        """
+        gets the oToken contract address for the reserve
+        :param _reserve: address of the reserve
+        :return:address of the oToken contract
+        """
 
     @external(readonly=True)
-    def getReserveTotalLiquidity(_reserve:Address)->int:
+    def getReserveAvailableLiquidity(self, _reserve: Address) -> int:
+        """
+        gets the available liquidity in the reserve. The available liquidity is the balance of the core contract
+        :param _reserve: address of the reserve
+        :return: the available liquidity
+        """
 
     @external(readonly=True)
-    def getReserveConfiguration(_reserve:Address)->dict:
-    
+    def getReserveTotalLiquidity(self, _reserve: Address) -> int:
+        """
+        gets the total liquidity in the reserve. The total liquidity is the balance of the core contract + total borrows
+        :param _reserve:address of the reserve
+        :return:the total liquidity
+        """
+
     @external(readonly=True)
-    def getReserves()->list:
-    
+    def getReserveConfiguration(self, _reserve: Address) -> dict:
+        """
+        this function aggregates the configuration parameters of the reserve.
+        It's used in the LendingPoolDataProvider specifically to save gas, and avoid multiple external contract calls to fetch the same data.
+        :param _reserve:address of the reserve
+        :return:a dict with reserve decimals,the base ltv as collateral,the liquidation threshold,if the reserve is used as collateral or not
+        """
 
-
+    @external(readonly=True)
+    def getReserves(self) -> list:
+        """
+        list of reserves
+        :return:the list of reserves configured on the core
+        """
