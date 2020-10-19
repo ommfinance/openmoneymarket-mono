cf0e5a591c7d482afc931e0c46a709a5138d93d2
kailehok <baral.kailash12345@gmail.com>
2020-10-19 22:22:09 +0545

    added dummy score for price oracle

---
+++ score/priceOracle/priceOracle.py
@@ -0,0 +1,24 @@
+from iconservice import *
+
+TAG = 'PriceOracle'
+
+
+class PriceOracle(IconScoreBase):
+
+    def __init__(self, db: IconScoreDatabase) -> None:
+        super().__init__(db)
+        self._price = DictDB( 'price', db, value_type = int, depth = 2)
+
+    def on_install(self) -> None:
+        super().on_install()
+
+    def on_update(self) -> None:
+        super().on_update()
+
+    @external
+    def set_reference_data(self, _base: str, _quote: str, _rate: int) -> None:
+        self._price[_base][_quote] = _rate
+    
+    @external(readonly = True)
+    def get_reference_data(self, _base: str, _quote) -> int:
+        return self._price[_base][_quote]
