c3926a10d902c0092d3a8e6127139bd3d3c63c10
shubham <shubh.tiwari512@gmail.com>
2021-01-15 14:48:21 +0545

    fromatted file

--- score/addressProvider/addressProvider.py
+++ score/addressProvider/addressProvider.py
@@ -7,20 +7,20 @@ class AddressProvider(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self._USDb = VarDB('USDb', db, value_type = Address)
-        self._sICX = VarDB('sICX', db, value_type = Address)
-        self._oUSDb = VarDB('oUSDb', db, value_type = Address)
-        self._oICX = VarDB('oICX', db, value_type = Address)
-        self._lendingPool = VarDB('lendingPool', db, value_type = Address)
-        self._lendingPoolDataProvider = VarDB('lendingPoolDataProvider', db, value_type = Address)
-        self._staking = VarDB('staking',db,value_type=Address)
+        self._USDb = VarDB('USDb', db, value_type=Address)
+        self._sICX = VarDB('sICX', db, value_type=Address)
+        self._oUSDb = VarDB('oUSDb', db, value_type=Address)
+        self._oICX = VarDB('oICX', db, value_type=Address)
+        self._lendingPool = VarDB('lendingPool', db, value_type=Address)
+        self._lendingPoolDataProvider = VarDB('lendingPoolDataProvider', db, value_type=Address)
+        self._staking = VarDB('staking', db, value_type=Address)
 
     def on_install(self) -> None:
         super().on_install()
 
     def on_update(self) -> None:
         super().on_update()
-    
+
     @external
     def setLendingPool(self, _address: Address) -> None:
         if self.msg.sender != self.owner:
@@ -57,38 +57,27 @@ class AddressProvider(IconScoreBase):
             revert(f'Only owner can set the address')
         self._oICX.set(_address)
 
-
     @external
     def setStaking(self, _address: Address) -> None:
         if self.msg.sender != self.owner:
             revert(f'Only owner can set the address')
         self._staking.set(_address)
 
-   
-
-    @external(readonly = True)
+    @external(readonly=True)
     def getAllAddresses(self) -> dict:
-        response = {"collateral" : {
-                    "USDb":self._USDb.get(),
-                    "sICX":self._sICX.get()
-                    },
-                    "oTokens": {
-                        "oUSDb": self._oUSDb.get(),
-                        "oICX": self._oICX.get()
-                    },
-                    "systemContract":{
-                        "LendingPool": self._lendingPool.get(),
-                        "LendingPoolDataProvider": self._lendingPoolDataProvider.get(),
-                        "Staking":self._staking.get()
-                    }
-                    }
-      
+        response = {"collateral": {
+            "USDb": self._USDb.get(),
+            "sICX": self._sICX.get()
+        },
+            "oTokens": {
+                "oUSDb": self._oUSDb.get(),
+                "oICX": self._oICX.get()
+            },
+            "systemContract": {
+                "LendingPool": self._lendingPool.get(),
+                "LendingPoolDataProvider": self._lendingPoolDataProvider.get(),
+                "Staking": self._staking.get()
+            }
+        }
+
         return response
-
-
-
-
-
-
-    
-    
