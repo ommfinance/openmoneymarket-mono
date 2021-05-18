61f57ba12f939a29452e6d73a1f6332b8c691e6a
shubham <shubh.tiwari512@gmail.com>
2021-05-18 13:42:27 +0545

    elaborate revert messages

--- score/delegation/utils/checks.py
+++ score/delegation/utils/checks.py
@@ -1,14 +1,18 @@
 from iconservice import *
 
+TAG = 'Delegation'
+
 
 def only_owner(func):
-	if not isfunction(func):
-		revert('NotAFunctionError')
+    if not isfunction(func):
+        revert(f'{TAG}'
+               'NotAFunctionError')
 
-	@wraps(func)
-	def __wrapper(self: object, *args, **kwargs):
-		if self.msg.sender != self.owner:
-			revert('SenderNotScoreOwnerError')
-		return func(self, *args, **kwargs)
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.owner:
+            revert(f'{TAG}'
+                   'SenderNotScoreOwnerError')
+        return func(self, *args, **kwargs)
 
-	return __wrapper
+    return __wrapper
