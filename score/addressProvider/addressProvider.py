226f1bc2d041b3523f7b77324d6c036c13666ee7
lilixac <hinewton25@gmail.com>
2021-06-11 15:31:23 +0545

    Updated to include debt tokens and usdb -> usds

--- score/addressProvider/addressProvider.py
+++ score/addressProvider/addressProvider.py
@@ -2,12 +2,15 @@ from iconservice import *
 
 
 class AddressProvider(IconScoreBase):
-    USDb = 'usdb'
+    USDs = 'usds'
     sICX = 'sicx'
-    oICX = 'oICX'
-    oUSDb = 'ousdb'
     IUSDC = 'iusdc'
+    oICX = 'oICX'
+    oUSDs = 'ousds'
     oIUSDC = 'oiusdc'
+    dICX = 'dICX'
+    dUSDs = 'dusds'
+    dIUSDC = 'diusdc'
     LENDING_POOL = 'lendingPool'
     LENDING_POOL_DATA_PROVIDER = 'lendingPoolDataProvider'
     STAKING = 'staking'
@@ -46,20 +49,28 @@ class AddressProvider(IconScoreBase):
         self._set_address(self.LENDING_POOL_DATA_PROVIDER, _address)
 
     @external
-    def setUSDb(self, _address: Address) -> None:
-        self._set_address(self.USDb, _address)
+    def setUSDs(self, _address: Address) -> None:
+        self._set_address(self.USDs, _address)
 
     @external
     def setsICX(self, _address: Address) -> None:
         self._set_address(self.sICX, _address)
 
     @external
-    def setoUSDb(self, _address: Address) -> None:
-        self._set_address(self.oUSDb, _address)
+    def setoUSDs(self, _address: Address) -> None:
+        self._set_address(self.oUSDs, _address)
+
+    @external
+    def setdUSDs(self, _address: Address) -> None:
+        self._set_address(self.dUSDs, _address)
 
     @external
     def setoICX(self, _address: Address) -> None:
         self._set_address(self.oICX, _address)
+
+    @external
+    def setdICX(self, _address: Address) -> None:
+        self._set_address(self.dICX, _address)
 
     @external
     def setStaking(self, _address: Address) -> None:
@@ -72,6 +83,10 @@ class AddressProvider(IconScoreBase):
     @external
     def setoIUSDC(self, _address: Address) -> None:
         self._set_address(self.oIUSDC, _address)
+
+    @external
+    def setdIUSDC(self, _address: Address) -> None:
+        self._set_address(self.dIUSDC, _address)
 
     @external
     def setOmmToken(self, _address: Address) -> None:
@@ -89,14 +104,19 @@ class AddressProvider(IconScoreBase):
     def getAllAddresses(self) -> dict:
         return {
             "collateral": {
-                "USDS": self._get_address(self.USDb),
+                "USDS": self._get_address(self.USDs),
                 "sICX": self._get_address(self.sICX),
                 "IUSDC": self._get_address(self.IUSDC),
             },
             "oTokens": {
-                "oUSDS": self._get_address(self.oUSDb),
+                "oUSDS": self._get_address(self.oUSDs),
                 "oICX": self._get_address(self.oICX),
                 "oIUSDC": self._get_address(self.oIUSDC),
+            },
+            "dTokens": {
+                "dUSDS": self._get_address(self.dUSDs),
+                "dICX": self._get_address(self.dICX),
+                "dIUSDC": self._get_address(self.dIUSDC),
             },
             "systemContract": {
                 "LendingPool": self._get_address(self.LENDING_POOL),
