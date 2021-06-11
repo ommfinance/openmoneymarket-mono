bf24e17b8e5b9f9911530d80879f410beb6774e4
shubham <shubh.tiwari512@gmail.com>
2021-06-11 12:39:29 +0545

    added dToken code

---
+++ score/dToken/utils/checks.py
@@ -0,0 +1,55 @@
+from iconservice import *
+
+TAG = "DToken"
+
+
+def only_lending_pool(func):
+    if not isfunction(func):
+        revert(f"{TAG}: ""NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.getLendingPool():
+            revert(f"{TAG}: "f"SenderNotAuthorized: (sender){self.msg.sender} (liquidation){self.getLendingPool()}")
+        return func(self, *args, **kwargs)
+
+    return __wrapper
+
+
+def only_lending_pool_core(func):
+    if not isfunction(func):
+        revert(f"{TAG}: ""NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.getLendingPoolCore():
+            revert(f"{TAG}: "f"SenderNotAuthorized: (sender){self.msg.sender} (liquidation){self.getLendingPoolCore()}")
+        return func(self, *args, **kwargs)
+
+    return __wrapper
+
+
+def only_liquidation(func):
+    if not isfunction(func):
+        revert(f"{TAG}: ""NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.getLiquidation():
+            revert(f"{TAG}: "f"SenderNotAuthorized: (sender){self.msg.sender} (liquidation){self.getLiquidation()}")
+        return func(self, *args, **kwargs)
+
+    return __wrapper
+
+
+def only_owner(func):
+    if not isfunction(func):
+        revert(f"{TAG}: ""NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.owner:
+            revert(f"{TAG}: "f"SenderNotScoreOwnerError: (sender){self.msg.sender} (owner){self.owner}")
+        return func(self, *args, **kwargs)
+
+    return __wrapper
