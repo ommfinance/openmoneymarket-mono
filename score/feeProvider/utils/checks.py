d19efd8d9aaf9a7f6e886afb3e297e202e935d63
shubham <shubh.tiwari512@gmail.com>
2021-06-25 17:00:44 +0545

    add feeProvider

---
+++ score/feeProvider/utils/checks.py
@@ -0,0 +1,17 @@
+from iconservice import *
+
+TAG = 'LendingPool'
+
+
+def only_owner(func):
+    if not isfunction(func):
+        revert(f"{TAG}: NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.owner:
+            revert(f"{TAG}: SenderNotScoreOwnerError: (sender){self.msg.sender} (owner){self.owner}")
+
+        return func(self, *args, **kwargs)
+
+    return __wrapper
