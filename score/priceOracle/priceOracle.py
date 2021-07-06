f4b44fb367fdde6657c3c85d5e6da90b7fc19089
srmaharjan <maharzan.sr@gmail.com>
2021-07-06 09:36:11 +0545

    calculate omm token price based on price of OMM/USDS OMM/IUSDC OMM/sICX

--- score/priceOracle/priceOracle.py
+++ score/priceOracle/priceOracle.py
@@ -1,4 +1,23 @@
+from .utils.Math import convertToExa, exaMul
 from .utils.checks import *
+
+OMM_TOKENS = [
+    {
+        "name": "OMM/USDS",
+        "base": "USDS",
+        "convert": lambda _amount, _source: _amount
+    },
+    {
+        "name": "OMM/sICX",
+        "base": "ICX",
+        "convert": lambda _amount, _source: exaMul(_amount, _source.getPriceByName("sICX/ICX"))
+    },
+    {
+        "name": "OMM/IUSDC",
+        "base": "IUSDC",
+        "convert": lambda _amount, _source: convertToExa(_amount, 6)
+    }
+]
 
 
 class OracleInterface(InterfaceScore):
@@ -7,16 +26,24 @@ class OracleInterface(InterfaceScore):
         pass
 
 
+class DataSourceInterface(InterfaceScore):
+    @interface
+    def getPriceByName(self, _name: str) -> int:
+        pass
+
+
 class PriceOracle(IconScoreBase):
     _PRICE = 'price'
     _BAND_ORACLE = 'bandOracle'
     _ORACLE_PRICE_BOOL = 'oraclePriceFeed'
+    _DATA_SOURCE = 'data_source'
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
         self._price = DictDB(self._PRICE, db, value_type=int, depth=2)
         self._bandOracle = VarDB(self._BAND_ORACLE, db, value_type=Address)
         self._oraclePriceBool = VarDB(self._ORACLE_PRICE_BOOL, db, value_type=bool)
+        self._data_source = VarDB(self._DATA_SOURCE, db, value_type=Address)
 
     def on_install(self) -> None:
         super().on_install()
@@ -45,21 +72,42 @@ class PriceOracle(IconScoreBase):
     def getBandOracle(self) -> Address:
         return self._bandOracle.get()
 
+    @external()
+    @only_owner
+    def setDataSource(self, _address: Address):
+        self._data_source.set(_address)
+
+    def getDataSource(self) -> Address:
+        return self._data_source.get()
+
     @only_owner
     @external
     def set_reference_data(self, _base: str, _quote: str, _rate: int) -> None:
         self._price[_base][_quote] = _rate
 
-    @external(readonly=True)
-    def get_reference_data(self, _base: str, _quote) -> int:
-        if _base == "USDS":
-            _base ="USDB"
+    def _get_price(self, _base: str, _quote) -> int:
         if self._oraclePriceBool.get():
-            if _base == "USDB":
-                return 10 ** 18
-            else:
-                oracle = self.create_interface_score(self._bandOracle.get(), OracleInterface)
-                price = oracle.get_reference_data(_base, _quote)
-                return price['rate']
+            oracle = self.create_interface_score(self._bandOracle.get(), OracleInterface)
+            price = oracle.get_reference_data(_base, _quote)
+            return price['rate']
         else:
             return self._price[_base][_quote]
+
+    def _get_omm_price(self, _quote) -> int:
+        lp_token = self.create_interface_score(self.getDataSource(), DataSourceInterface)
+        _total_price = 0
+        for token in OMM_TOKENS:
+            name = token["name"]
+            base = token["base"]
+            _price = lp_token.getPriceByName(name)
+            _adjusted_price = token["convert"](_price, lp_token)
+            _total_price += exaMul(_adjusted_price, self._get_price(base, _quote))
+
+        return _total_price // len(OMM_TOKENS)
+
+    @external(readonly=True)
+    def get_reference_data(self, _base: str, _quote) -> int:
+        if _base == 'OMM':
+            return self._get_omm_price(_quote)
+        else:
+            return self._get_price(_base, _quote)
---
+++ score/tests/unit_test/test_unit_price_oracle.py
@@ -0,0 +1,53 @@
+from iconservice import Address
+from iconservice.base.exception import InvalidParamsException
+from tbears.libs.scoretest.patch.score_patcher import get_interface_score
+from tbears.libs.scoretest.score_test_case import ScoreTestCase
+from priceOracle.priceOracle import PriceOracle
+
+EXA = 10 ** 18
+
+
+class TestPriceOracle(ScoreTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self._owner = self.test_account1
+        self.score = self.get_score_instance(PriceOracle, self._owner)
+
+        self.mock_lp_address = Address.from_string(f"cx{'1231' * 10}")
+        self.mock_band_oracle = Address.from_string(f"cx{'1232' * 10}")
+
+        self.set_msg(self._owner)
+        self.score.setDataSource(self.mock_lp_address)
+        self.score.setBandOracle(self.mock_band_oracle)
+
+    def test_get_reference_data_for_omm(self):
+        self.set_msg(self._owner)
+        # self.score.toggleOraclePriceBool()
+
+        self.score.set_reference_data("USDS", "USD", 15 * EXA // 10)
+        self.score.set_reference_data("IUSDC", "USD", 12 * EXA // 10)
+        self.score.set_reference_data("ICX", "USD", 5 * EXA // 10)
+        self.set_msg(None)
+
+        def _price_side_effect(_name):
+            if _name == 'OMM/USDS':
+                return 14 * EXA // 10
+            elif _name == 'OMM/IUSDC':
+                return 15 * 10 ** 6 / 10
+            elif _name == 'OMM/sICX':
+                return 75 * EXA // 100
+            elif _name == 'sICX/ICX':
+                return 85 * EXA // 100
+            else:
+                raise InvalidParamsException(f"Invalid parameter {_name}")
+
+        self.patch_internal_method(self.mock_lp_address, "getPriceByName", _price_side_effect)
+
+        actual_result = self.score.get_reference_data("OMM", "USD")
+
+        _mock_dex_score = get_interface_score(self.mock_lp_address)
+
+        self.assertEqual(4, _mock_dex_score.getPriceByName.call_count)
+
+        self.assertEqual(140625 * EXA // 100000, actual_result)
