552a2bb1580aa3f49b147d3eeb1fc6f64d61cfc9
srmaharjan <maharzan.sr@gmail.com>
2021-07-07 15:44:14 +0545

    use token for decimal reference

--- score/priceOracle/priceOracle.py
+++ score/priceOracle/priceOracle.py
@@ -1,21 +1,24 @@
 from .utils.Math import convertToExa, exaMul
 from .utils.checks import *
 
+OMM_TOKEN_PREFIX = "OMM/"
+
 OMM_TOKENS = [
     {
-        "name": "OMM/USDS",
-        "base": "USDS",
-        "convert": lambda _amount, _source: _amount
+        "name": "USDS",
+        "priceOracleKey": "USDS",
+        "convert": lambda _source, _amount, _decimals: convertToExa(_amount, _decimals)
     },
     {
-        "name": "OMM/sICX",
-        "base": "ICX",
-        "convert": lambda _amount, _source: exaMul(_amount, _source.getPriceByName("sICX/ICX"))
+        "name": "sICX",
+        "priceOracleKey": "ICX",
+        "convert": lambda _source, _amount, _decimals: exaMul(convertToExa(_amount, _decimals),
+                                                              _source.getPriceByName("sICX/ICX"))
     },
     {
-        "name": "OMM/IUSDC",
-        "base": "IUSDC",
-        "convert": lambda _amount, _source: convertToExa(_amount, 6)
+        "name": "IUSDC",
+        "priceOracleKey": "USDC",
+        "convert": lambda _source, _amount, _decimals: convertToExa(_amount, _decimals)
     }
 ]
 
@@ -26,9 +29,21 @@ class OracleInterface(InterfaceScore):
         pass
 
 
+class AddressProviderInterface(InterfaceScore):
+    @interface
+    def getCollateralAddresses(self) -> dict:
+        pass
+
+
 class DataSourceInterface(InterfaceScore):
     @interface
     def getPriceByName(self, _name: str) -> int:
+        pass
+
+
+class TokenInterface(InterfaceScore):
+    @interface
+    def decimals(self) -> int:
         pass
 
 
@@ -37,6 +52,7 @@ class PriceOracle(IconScoreBase):
     _BAND_ORACLE = 'bandOracle'
     _ORACLE_PRICE_BOOL = 'oraclePriceFeed'
     _DATA_SOURCE = 'data_source'
+    _ADDRESS_PROVIDER = 'address_provider'
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
@@ -44,6 +60,7 @@ class PriceOracle(IconScoreBase):
         self._bandOracle = VarDB(self._BAND_ORACLE, db, value_type=Address)
         self._oraclePriceBool = VarDB(self._ORACLE_PRICE_BOOL, db, value_type=bool)
         self._data_source = VarDB(self._DATA_SOURCE, db, value_type=Address)
+        self._address_provider = VarDB(self._ADDRESS_PROVIDER, db, value_type=Address)
 
     def on_install(self) -> None:
         super().on_install()
@@ -80,13 +97,24 @@ class PriceOracle(IconScoreBase):
     def getDataSource(self) -> Address:
         return self._data_source.get()
 
+    @external()
+    @only_owner
+    def setAddressProvider(self, _address: Address):
+        self._address_provider.set(_address)
+
+    def getAddressProvider(self) -> Address:
+        return self._address_provider.get()
+
     @only_owner
     @external
     def set_reference_data(self, _base: str, _quote: str, _rate: int) -> None:
         self._price[_base][_quote] = _rate
 
     def _get_price(self, _base: str, _quote) -> int:
-        if self._oraclePriceBool.get():
+        # TODO remove me for main-net deployment
+        if _base == "USDS":
+            _base = "USDB"
+        if self._oraclePriceBool.get() or _base == 'ICX':
             oracle = self.create_interface_score(self._bandOracle.get(), OracleInterface)
             price = oracle.get_reference_data(_base, _quote)
             return price['rate']
@@ -95,13 +123,21 @@ class PriceOracle(IconScoreBase):
 
     def _get_omm_price(self, _quote) -> int:
         lp_token = self.create_interface_score(self.getDataSource(), DataSourceInterface)
+        address_provider = self.create_interface_score(self.getAddressProvider(), AddressProviderInterface)
+        collateral_addresses = address_provider.getCollateralAddresses()
+
         _total_price = 0
         for token in OMM_TOKENS:
             name = token["name"]
-            base = token["base"]
-            _price = lp_token.getPriceByName(name)
-            _adjusted_price = token["convert"](_price, lp_token)
-            _total_price += exaMul(_adjusted_price, self._get_price(base, _quote))
+            price_oracle_key = token["priceOracleKey"]
+
+            _price = lp_token.getPriceByName(f"{OMM_TOKEN_PREFIX}{name}")
+
+            _interface = self.create_interface_score(collateral_addresses[name], TokenInterface)
+            _decimals = _interface.decimals()
+
+            _adjusted_price = token["convert"](lp_token, _price, _decimals)
+            _total_price += exaMul(_adjusted_price, self._get_price(price_oracle_key, _quote))
 
         return _total_price // len(OMM_TOKENS)
 
--- score/tests/unit_test/test_unit_price_oracle.py
+++ score/tests/unit_test/test_unit_price_oracle.py
@@ -16,19 +16,34 @@ class TestPriceOracle(ScoreTestCase):
 
         self.mock_lp_address = Address.from_string(f"cx{'1231' * 10}")
         self.mock_band_oracle = Address.from_string(f"cx{'1232' * 10}")
+        self.mock_address_provider = Address.from_string(f"cx{'1234' * 10}")
 
         self.set_msg(self._owner)
         self.score.setDataSource(self.mock_lp_address)
         self.score.setBandOracle(self.mock_band_oracle)
+        self.score.setAddressProvider(self.mock_address_provider)
 
     def test_get_reference_data_for_omm(self):
         self.set_msg(self._owner)
         # self.score.toggleOraclePriceBool()
 
-        self.score.set_reference_data("USDS", "USD", 15 * EXA // 10)
+        self.score.set_reference_data("USDB", "USD", 15 * EXA // 10)
         self.score.set_reference_data("IUSDC", "USD", 12 * EXA // 10)
-        self.score.set_reference_data("ICX", "USD", 5 * EXA // 10)
         self.set_msg(None)
+
+        self.patch_internal_method(self.mock_band_oracle, "get_reference_data",
+                                   lambda _base, _quote: {"rate": 5 * EXA // 10})
+
+        _collateral_address = {
+            "USDS": Address.from_string(f"cx{'9841' * 10}"),
+            "sICX": Address.from_string(f"cx{'9842' * 10}"),
+            "IUSDC": Address.from_string(f"cx{'9843' * 10}")
+        }
+
+        self.patch_internal_method(self.mock_address_provider, "getCollateralAddresses", lambda: _collateral_address)
+        self.patch_internal_method(_collateral_address["USDS"], "decimals", lambda: 18)
+        self.patch_internal_method(_collateral_address["sICX"], "decimals", lambda: 18)
+        self.patch_internal_method(_collateral_address["IUSDC"], "decimals", lambda: 6)
 
         def _price_side_effect(_name):
             if _name == 'OMM/USDS':
@@ -51,3 +66,5 @@ class TestPriceOracle(ScoreTestCase):
         self.assertEqual(4, _mock_dex_score.getPriceByName.call_count)
 
         self.assertEqual(140625 * EXA // 100000, actual_result)
+
+        self.assert_internal_call(self.mock_band_oracle, "get_reference_data", "ICX", "USD")
