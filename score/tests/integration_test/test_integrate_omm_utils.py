3055fd410880862575326ddd5b4d938308bb1380
lilixac <hinewton25@gmail.com>
2021-07-16 10:36:06 +0545

    Tabs -> Spaces, Added helper methods

--- score/tests/integration_test/test_integrate_omm_utils.py
+++ score/tests/integration_test/test_integrate_omm_utils.py
@@ -3,153 +3,209 @@ from iconsdk.wallet.wallet import KeyWallet
 from .test_integrate_base import *
 
 class OmmUtils(OMMTestBase):
-	def setUp(self):
-		super().setUp()
+    def setUp(self):
+        super().setUp()
 
-	def _depositICX(self, _from, _depositAmount):
+    def _depositICX(self, _from, _depositAmount):
 
-		params = {"_amount": _depositAmount}
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["lendingPool"], 
-			value=_depositAmount,
-			method="deposit",
-			params=params
-			)
-		return tx_result
+        params = {"_amount": _depositAmount}
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["lendingPool"], 
+            value=_depositAmount,
+            method="deposit",
+            params=params
+            )
+        return tx_result
 
-	def _borrowICX(self, _from, _borrowAmount):
+    def _borrowICX(self, _from, _borrowAmount):
 
-		params ={"_reserve": self.contracts['sicx'], "_amount": _borrowAmount}
+        params ={"_reserve": self.contracts['sicx'], "_amount": _borrowAmount}
 
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["lendingPool"],
-			method="borrow",
-			params=params
-			)
-		return tx_result
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["lendingPool"],
+            method="borrow",
+            params=params
+            )
+        return tx_result
 
-	def _redeemICX(self, _from, _redeemAmount):
+    def _redeemICX(self, _from, _redeemAmount):
 
-		params = {
-			"_oToken": self.contracts["oICX"],
-			"_amount": _redeemAmount,
-		}
+        params = {
+            "_oToken": self.contracts["oICX"],
+            "_amount": _redeemAmount,
+        }
 
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["lendingPool"],
-			method="redeem",
-			params=params
-			)
-		return tx_result
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["lendingPool"],
+            method="redeem",
+            params=params
+            )
+        return tx_result
 
-	def _repayICX(self, _from, _repayAmount):
+    def _repayICX(self, _from, _repayAmount):
 
-		repay_data = {'method': 'repay', 'params': {'amount': _repayAmount}}
-		data = json.dumps(repay_data).encode('utf-8')
+        repay_data = {'method': 'repay', 'params': {'amount': _repayAmount}}
+        data = json.dumps(repay_data).encode('utf-8')
 
-		params = {"_to": self.contracts['lendingPool'],
-					"_value": _repayAmount, 
-					"_data": data}
+        params = {"_to": self.contracts['lendingPool'],
+                    "_value": _repayAmount, 
+                    "_data": data}
 
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["sicx"],
-			method="transfer",
-			params=params
-			)
-		return tx_result
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["sicx"],
+            method="transfer",
+            params=params
+            )
+        return tx_result
 
-	def _borrowUSDS(self, _from, _borrowAmount):
+    def _borrowUSDS(self, _from, _borrowAmount):
 
-		params = {
-			"_reserve": self.contracts['usds'],
-			"_amount": _borrowAmount,
-		}
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["lendingPool"],
-			method="borrow",
-			params=params
-			)
-		return tx_result
+        params = {
+            "_reserve": self.contracts['usds'],
+            "_amount": _borrowAmount,
+        }
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["lendingPool"],
+            method="borrow",
+            params=params
+            )
+        return tx_result
 
-	def _depositUSDS(self, _from, _depositAmount):
+    def _depositUSDS(self, _from, _depositAmount):
 
-		depositData = {'method': 'deposit', 'params': {'amount': _depositAmount}}
+        depositData = {'method': 'deposit', 'params': {'amount': _depositAmount}}
 
-		data = json.dumps(depositData).encode('utf-8')
-		params = {"_to": self.contracts['lendingPool'],
-				"_value": _depositAmount, 
-				"_data": data}
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["usds"], #USDB contract
-			method="transfer",
-			params=params
-			)
-		return tx_result
+        data = json.dumps(depositData).encode('utf-8')
+        params = {"_to": self.contracts['lendingPool'],
+                "_value": _depositAmount, 
+                "_data": data}
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["usds"], #USDB contract
+            method="transfer",
+            params=params
+            )
+        return tx_result
 
-	def _mintUSDS(self, _from, _to):
-		params = {
-			"_to": _to,
-			"_value": 1000 * 10 ** 18
-		}
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["usds"],
-			method="mint",
-			params=params
-			)
-		return tx_result
+    def _mintUSDS(self, _from, _to):
+        params = {
+            "_to": _to,
+            "_value": 1000 * 10 ** 18
+        }
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["usds"],
+            method="mint",
+            params=params
+            )
+        return tx_result
 
-	def _redeemUSDS(self, _from, _redeemAmount):
+    def _redeemUSDS(self, _from, _redeemAmount):
 
-		params = {
-				"_oToken": self.contracts["oUSDS"],
-				"_amount": _redeemAmount,
-				}
+        params = {
+                "_oToken": self.contracts["oUSDS"],
+                "_amount": _redeemAmount,
+                }
 
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["lendingPool"],
-			method="redeem",
-			params=params
-			)
-		return tx_result
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["lendingPool"],
+            method="redeem",
+            params=params
+            )
+        return tx_result
 
-	def _repayUSDS(self, _from, _repayAmount):
+    def _repayUSDS(self, _from, _repayAmount):
 
-		repay_data = {'method': 'repay', 'params': {'amount': _repayAmount}}
-		data = json.dumps(repay_data).encode('utf-8')
+        repay_data = {'method': 'repay', 'params': {'amount': _repayAmount}}
+        data = json.dumps(repay_data).encode('utf-8')
 
-		params = {"_to": self.contracts['lendingPool'],
-				"_value": _repayAmount, 
-				"_data": data}
+        params = {"_to": self.contracts['lendingPool'],
+                "_value": _repayAmount, 
+                "_data": data}
 
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["usds"], #USDB contract
-			method="transfer",
-			params=params
-			)
-		return tx_result
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["usds"], #USDB contract
+            method="transfer",
+            params=params
+            )
+        return tx_result
 
-	def _transferUSDS(self, _from, _to, _transferAmount):
+    def _transferUSDS(self, _from, _to, _transferAmount):
 
-		params = {
-			"_to": _to,
-			"_value": _transferAmount
-		}
+        params = {
+            "_to": _to,
+            "_value": _transferAmount
+        }
 
-		tx_result = self.send_tx(
-			from_=_from,
-			to=self.contracts["usds"], #USDS contract
-			method="transfer",
-			params=params
-			)
-		return tx_result
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts["usds"], #USDS contract
+            method="transfer",
+            params=params
+            )
+        return tx_result
+
+    def _mintLpTokens(self, _from, _to, _id):
+        params = {
+            "_to": _to,
+            "_id": _id
+        }
+
+        tx_result = self.send_tx(
+            from_=self.deployer_wallet,
+            to=self.contracts["lpToken"], #USDS contract
+            method="mint",
+            params=params
+            )
+        return tx_result
+
+    def _stakeLp(self, _from, _amount, _id):
+        data = {
+            'method': 'stake',
+            'params': ''
+        }
+
+        data = json.dumps(data).encode('utf-8')
+
+        params = {
+            '_to': self.contracts['stakedLp'],
+            '_value': _amount,
+            '_id': _id,
+            '_data': data
+        }
+
+        tx_result = self.send_tx(
+            from_=_from,
+            to=self.contracts['lpToken'],
+            method="transfer",
+            params=params
+            )
+
+        return tx_result
+
+    def _unstakeLp(self, _from, _amount, _id):
+        params = {
+            '_id': _id, 
+            '_value':_amount
+        }
+
+        tx_result = self.send_tx(
+                from_=_from,
+                to=self.contracts['stakedLp'],
+                method="unstake",
+                params=params
+            )
+
+        return tx_result
+
+
+
 
 
