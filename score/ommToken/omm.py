c3e55ba721e1d736977e5ca425001e81b718de83
srmaharjan <maharzan.sr@gmail.com>
2021-07-25 21:32:18 +0545

    add pool to reward through omm and stakedLp

--- score/ommToken/omm.py
+++ score/ommToken/omm.py
@@ -1,6 +1,8 @@
 from .tokens.IRC2mintable import IRC2Mintable
 from iconservice import *
 
+from .utils.checks import only_owner
+
 TOKEN_NAME = 'OmmToken'
 SYMBOL_NAME = 'OMM'
 
@@ -8,6 +10,20 @@ SYMBOL_NAME = 'OMM'
 class SupplyDetails(TypedDict):
     principalUserBalance: int
     principalTotalSupply: int
+
+
+class AssetConfig(TypedDict):
+    _id: int
+    asset: Address
+    distPercentage: int
+    assetName: str
+    mapping: str
+
+
+class RewardDistributionInterface(InterfaceScore):
+    @interface
+    def configureLPEmission(self, _assetConfig: List[AssetConfig]) -> None:
+        pass
 
 
 class OmmToken(IRC2Mintable):
@@ -29,3 +45,16 @@ class OmmToken(IRC2Mintable):
         :return: total staked balance
         """
         return self.total_staked_balance()
+
+    @only_owner
+    @external
+    def addPool(self, _distPercentage: int, assetName: str = "OMM") -> None:
+        reward = self.create_interface_score(self._addresses["rewards"], RewardDistributionInterface)
+        _config = {
+            "_id": -1,
+            "asset": self.address,
+            "distPercentage": _distPercentage,
+            "assetName": assetName,
+            "mapping": "liquidityProvider",
+        }
+        reward.configureLPEmission([_config])
