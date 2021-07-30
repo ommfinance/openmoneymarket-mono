6fa4c37d40c3240917a27d11f7435a05f5b5e57e
srmaharjan <maharzan.sr@gmail.com>
2021-07-30 09:05:36 +0545

    update api for distribution percentage for all reserve and pool api add sub-total and total in response

--- score/rewardDistribution/rewardConfigurationDB.py
+++ score/rewardDistribution/rewardConfigurationDB.py
@@ -126,13 +126,24 @@ class RewardConfigurationDB(object):
         return _overallPercentage
 
     def getAssetConfigs(self) -> dict:
-        configs = {"liquidity": {}, "staking": {}, "reserve": {}}
+        _total_percentage = 0
+        response = {}
         for asset in self._assets:
             _name = self._assetName[asset]
             _percentage = self.getAssetPercentage(asset)
             _entity = self.getEntity(asset)
-            configs[_entity][_name] = _percentage
-        return configs
+
+            _entityMap = {} if response.get(_entity) is None else response.get(_entity)
+            total = 0 if response.get("total") is None else _entityMap.get(_entity)
+
+            _entityMap[_name] = _percentage
+            _entityMap["total"] = total + _percentage
+            response[_entity] = _entityMap
+            _total_percentage += _percentage
+
+        response['total'] = _total_percentage
+
+        return response
 
     def assetConfigOfLiquidityProvider(self) -> dict:
         configs = {}
