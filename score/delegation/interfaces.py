db0ab52f94ac9808536cded5915e98deda47b0ca
shubham <shubh.tiwari512@gmail.com>
2021-08-02 14:04:09 +0545

    added interfaces to separate file

---
+++ score/delegation/interfaces.py
@@ -0,0 +1,29 @@
+from iconservice import *
+
+
+class AddressDetails(TypedDict):
+    name: str
+    address: Address
+
+
+class PrepDelegations(TypedDict):
+    _address: Address
+    _votes_in_per: int
+
+
+class SystemInterface(InterfaceScore):
+    @interface
+    def getPRep(self, address: Address) -> dict:
+        pass
+
+
+class OmmTokenInterface(InterfaceScore):
+    @interface
+    def details_balanceOf(self, _owner: Address) -> dict:
+        pass
+
+
+class LendingPoolCoreInterface(InterfaceScore):
+    @interface
+    def updatePrepDelegations(self, _delegations: List[PrepDelegations]):
+        pass
