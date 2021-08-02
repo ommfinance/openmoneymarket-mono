d939d4d0043dfb8befb4f5d8c13cc49c5a9886ad
izyak <lbackup887@gmail.com>
2021-08-02 13:14:11 +0545

    #25 Avoid interscore call for data we already have

--- score/lendingPool/lendingPool.py
+++ score/lendingPool/lendingPool.py
@@ -386,7 +386,7 @@ class LendingPool(IconScoreBase):
         # if self.msg.sender != reserveData['oTokenAddress']:
         #     revert(f'{TAG}: {self.msg.sender} is unauthorized to call, only otoken can invoke the method')
 
-        reserveAvailableLiquidity = core.getReserveAvailableLiquidity(_reserve)
+        reserveAvailableLiquidity = reserveData.get('availableLiquidity')
         if reserveAvailableLiquidity < _amount:
             revert(f'{TAG}: Amount {_amount} is more than available liquidity {reserveAvailableLiquidity}')
 
