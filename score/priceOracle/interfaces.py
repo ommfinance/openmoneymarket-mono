f95038c51539bd5951f1b4912fdc061d06d12a0d
lilixac <hinewton25@gmail.com>
2021-08-03 13:19:53 +0545

    Add interfaces and addresses to new file

---
+++ score/priceOracle/interfaces.py
@@ -0,0 +1,32 @@
+from iconservice import *
+
+class AddressDetails(TypedDict):
+    name: str
+    address: Address
+
+
+class OracleInterface(InterfaceScore):
+    @interface
+    def get_reference_data(self, _base: str, _quote: str) -> dict:
+        pass
+
+
+class DataSourceInterface(InterfaceScore):
+    @interface
+    def lookupPid(self, _name: str) -> int:
+        pass
+
+    @interface
+    def getPoolStats(self, _id: int) -> dict:
+        pass
+
+    @interface
+    def getPriceByName(self, _name: str) -> int:
+        pass
+
+
+class TokenInterface(InterfaceScore):
+    @interface
+    def decimals(self) -> int:
+        pass
+
