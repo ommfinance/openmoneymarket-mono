d5dbcda4307c3a39565fcadb7a8a004672988f81
shubham <shubh.tiwari512@gmail.com>
2021-08-10 12:29:17 +0545

    added feeProvider configs

--- score/feeProvider/utils/checks.py
+++ score/feeProvider/utils/checks.py
@@ -10,8 +10,38 @@ def only_owner(func):
     @wraps(func)
     def __wrapper(self: object, *args, **kwargs):
         if self.msg.sender != self.owner:
-            revert(f"{TAG}: SenderNotScoreOwnerError: (sender){self.msg.sender} (owner){self.owner}")
+            revert(f"{TAG}:" f"SenderNotScoreOwnerError: (sender){self.msg.sender} (owner){self.owner}")
 
+        return func(self, *args, **kwargs)
+
+    return __wrapper
+
+
+def only_address_provider(func):
+    if not isfunction(func):
+        revert(f"{TAG}: ""NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        addressProvider = self._addressProvider.get()
+        if self.msg.sender != addressProvider:
+            revert(
+                f"{TAG}: "f"SenderNotAddressProviderError: (sender){self.msg.sender} (address provider){addressProvider}")
+        return func(self, *args, **kwargs)
+
+    return __wrapper
+
+
+def only_governance(func):
+    if not isfunction(func):
+        revert(f"{TAG}: ""NotAFunctionError")
+
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        governance = self._addresses['governance']
+        if self.msg.sender != addressProvider:
+            revert(
+                f"{TAG}: "f"SenderNotGovernanceError: (sender){self.msg.sender} (governance){governance}")
         return func(self, *args, **kwargs)
 
     return __wrapper
