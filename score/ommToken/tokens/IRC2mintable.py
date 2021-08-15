5ee160e6fecd1321ece8931af4f7ec21161eef82
shubham <shubh.tiwari512@gmail.com>
2021-08-15 11:33:36 +0545

    removed temporary method

--- score/ommToken/tokens/IRC2mintable.py
+++ score/ommToken/tokens/IRC2mintable.py
@@ -23,18 +23,3 @@ class IRC2Mintable(IRC2):
 			_data = b'minted by reward'
 		self._mint(self.msg.sender, _amount, _data)
 
-	@external
-	@only_owner
-	def mintTo(self, _to: Address, _amount: int, _data: bytes = None) -> None:
-		"""
-		Creates `_amount` number of tokens, and assigns to `_account`.
-		Increases the balance of that account and total supply.
-		See {IRC2-_mint}
-
-		:param _account: The account at which token is to be created.
-		:param _amount: Number of tokens to be created at the account.
-		:param _data:
-		"""
-		if _data is None:
-			_data = b'minted to by owner'
-		self._mint(_to, _amount, _data)
