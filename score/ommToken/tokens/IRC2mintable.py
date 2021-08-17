5fc432f96a731276dad213d2b7fee771fe5867a7
lilixac <hinewton25@gmail.com>
2021-08-17 11:29:38 +0545

    Add mint to method for tests

--- score/ommToken/tokens/IRC2mintable.py
+++ score/ommToken/tokens/IRC2mintable.py
@@ -23,3 +23,18 @@ class IRC2Mintable(IRC2):
 			_data = b'minted by reward'
 		self._mint(self.msg.sender, _amount, _data)
 
+	@external
+	@only_owner
+	def mintTo(self, _to: Address, _amount: int, _data: bytes = None) -> None:
+		"""
+		Creates `_amount` number of tokens, and assigns to caller account.
+		Increases the balance of that account and total supply.
+		See {IRC2-_mint}
+
+		:param _amount: Number of tokens to be created at the account.
+		:param _data:
+		"""
+		if _data is None:
+			_data = b'minted by reward'
+		self._mint(_to, _amount, _data)
+
