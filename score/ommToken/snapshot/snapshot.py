7d4aacf81e867b2d20772c01df828e92a054bc7e
srmaharjan <maharzan.sr@gmail.com>
2021-09-12 11:43:32 +0545

    implements snapshot db

---
+++ score/ommToken/snapshot/snapshot.py
@@ -0,0 +1,66 @@
+from iconservice import *
+
+from .snapshot_db import SnapshotDB
+
+
+class StakedDetail(TypedDict):
+    total_staked_balance: int
+    user_staked_balanced: int
+
+
+class Snapshot(IconScoreBase):
+
+    @abstractmethod
+    def on_install(self, **kwargs) -> None:
+        super().on_install(**kwargs)
+
+    @abstractmethod
+    def on_update(self, **kwargs) -> None:
+        super().on_update(**kwargs)
+
+    @abstractmethod
+    def __init__(self, _name: str, db: IconScoreDatabase) -> None:
+        super().__init__(db)
+        self._snapshot = SnapshotDB(_name, self.address, db)
+
+    @eventlog
+    def SnapshotCreated(self, _owner: Address, _user_staked: int):
+        pass
+
+    def _is_snapshot_exists(self, _owner: Address) -> bool:
+        return self._snapshot.is_snapshot_exists(_owner)
+
+    def _create_initial_snapshot(self, _owner: Address, _timestamp: int, _user_staked_balance: int) -> None:
+        """
+        create initial snapshot if not exists
+        :param _owner:
+        :param _timestamp:
+        :param _user_staked_balance:
+        :return: None
+        """
+        if not self._is_snapshot_exists(_owner) and _user_staked_balance > 0:
+            self._snapshot.create_checkpoints(_owner, _timestamp, _user_staked_balance)
+
+    def _createSnapshot(self, _owner: Address, _user_staked_balance: int, _total_staked_balance: int) -> None:
+        """
+        Create snapshot for _owner with curren staked
+        :param _owner: The address of the account to set
+        :return: None
+        """
+        _now = self.now()
+        self._snapshot.create_checkpoints(_owner, _now, _user_staked_balance)
+        self._snapshot.create_total_checkpoints(_now, _total_staked_balance)
+
+        self.SnapshotCreated(_owner, _user_staked_balance)
+
+    @abstractmethod
+    def stakedBalanceOfAt(self, _owner: Address, _timestamp: int) -> int:
+        return self._snapshot.get_staked_at(_owner, _timestamp)
+
+    @external(readonly=True)
+    def getTotalStakedBalanceOfAt(self, _timestamp: int) -> int:
+        return self._snapshot.get_total_staked(_timestamp)
+
+    @external
+    def totalStakedBalanceOfAt(self, _timestamp: int) -> int:
+        return self._snapshot.set_and_get_total_staked(_timestamp)
---
+++ score/ommToken/snapshot/snapshot_db.py
@@ -0,0 +1,119 @@
+from iconservice import *
+
+TAG = "SnapshotDB"
+
+
+class SnapshotDB(object):
+    _PREFIX = "SnapshotDB_"
+
+    def __init__(self, _key: str, _contract: Address, db: IconScoreDatabase):
+        # number of checkpoint for each address (address > checkpoint_count)
+        self._checkpoint_count = DictDB(f'{self._PREFIX}{_key}_checkpoint_count', db, value_type=int)
+        # checkpoints for marking number of timestamp (address > checkpoint_counter > timestamp)
+        self._timestamp_checkpoints = DictDB(f'{self._PREFIX}{_key}_timestamp_checkpoints', db, value_type=int, depth=2)
+        # checkpoints for marking number of votes (address > checkpoint_counter > staked)
+        self._staked_checkpoints = DictDB(f'{self._PREFIX}{_key}_staked_checkpoints', db, value_type=int, depth=2)
+
+        # checkpoints for marking number of total staked from a given timestamp (timestamp > total_staked)
+        self._total_staked_checkpoints = DictDB(f'{self._PREFIX}{_key}_total_staked_checkpoints', db, value_type=int)
+
+        self._contract = _contract
+
+    def is_snapshot_exists(self, _owner: Address) -> bool:
+        nCheckpoints = self._checkpoint_count[_owner]
+        return nCheckpoints > 0
+
+    def create_total_checkpoints(self, _timestamp: int, _staked: int):
+        self.create_checkpoints(self._contract, _timestamp, _staked)
+
+    def create_checkpoints(self, _owner: Address, _timestamp: int, _staked: int):
+        """
+         Create checkpoint for `_owner` at `_timestamp`
+        :param _owner: The address of the account to set
+        :param _timestamp: The timestamp number to set the vote balance at
+        :param _staked: The staked to set  at
+        :return: None
+        """
+        nCheckpoints = self._checkpoint_count[_owner]
+        if nCheckpoints > 0 and self._timestamp_checkpoints[_owner][nCheckpoints - 1] == _timestamp:
+            self._staked_checkpoints[_owner][nCheckpoints - 1] = _staked
+        else:
+            self._checkpoint_count[_owner] = nCheckpoints + 1
+            self._timestamp_checkpoints[_owner][nCheckpoints] = _timestamp
+            self._staked_checkpoints[_owner][nCheckpoints] = _staked
+
+    def get_current_staked(self, _owner: Address) -> int:
+        """
+        Gets the current staked balance for `_owner`
+        :param _owner: The address to get staked balance
+        :return: The number of current staked for `_owner`
+        """
+        nCheckpoints = self._checkpoint_count[_owner]
+        if nCheckpoints > 0:
+            return self._staked_checkpoints[_owner][nCheckpoints - 1]
+        return 0
+
+    def get_staked_at(self, _owner: Address, _timestamp: int) -> int:
+        """
+        Determine the prior number of staked for an account as of a timestamp number
+        :param _owner: The address of the account to check
+        :param _timestamp: The timestamp number to get the vote balance at
+        :return: The number of staked the account had as of the given timestamp
+        """
+        nCheckpoints = self._checkpoint_count[_owner]
+        if nCheckpoints == 0:
+            return 0
+        _latest_timestamp = self._timestamp_checkpoints[_owner][nCheckpoints - 1]
+        if _latest_timestamp <= _timestamp:
+            return self._staked_checkpoints[_owner][nCheckpoints - 1]
+        _initial_timestamp = self._timestamp_checkpoints[_owner][0]
+        # if _timestamp is less than _initial_timestamp, there is now staking for user
+        if _initial_timestamp > _timestamp:
+            return 0
+
+        if _initial_timestamp == _timestamp:
+            return self._staked_checkpoints[_owner][0]
+
+        return self._search_for_staked(nCheckpoints, _owner, _timestamp)
+
+    def _search_for_staked(self, _nCheckpoints: int, _owner: Address, _timestamp: int) -> int:
+        """
+        Binary search for staked
+        :param _nCheckpoints: Number of total checkpoints for `_owner`
+        :param _owner: The address of the account to check
+        :param _timestamp: The timestamp number to get the vote balance at
+        :return: The number of staked the account had as of the given timestamp or lower timestamp
+        """
+        _lower = 0
+        _mid = 0
+        _upper = _nCheckpoints - 1
+        while _upper > _lower:
+            _mid = -((_upper + _lower) // -2)  # ceil
+            _mid_timestamp = self._timestamp_checkpoints[_owner][_mid]
+            if _mid_timestamp == _timestamp:
+                return self._staked_checkpoints[_owner][_mid]
+            elif _mid_timestamp < _timestamp:
+                _lower = _mid
+            else:
+                _upper = _mid - 1
+        return self._staked_checkpoints[_owner][_lower]
+
+    def get_total_staked(self, _timestamp: int) -> int:
+        _staked = self._total_staked_checkpoints[_timestamp]
+        if _staked:
+            return _staked
+        return self.get_staked_at(self._contract, _timestamp)
+
+    def set_and_get_total_staked(self, _timestamp: int) -> int:
+        """
+        Determine the prior number of total staked for an account as of a timestamp number
+        :param _timestamp: The timestamp number to get the vote balance at
+        :return: The total number of staked had as of the given timestamp
+        """
+        _staked = self._total_staked_checkpoints[_timestamp]
+        if _staked:
+            return _staked
+
+        _total_staked_on_timestamp = self.get_staked_at(self._contract, _timestamp)
+        self._total_staked_checkpoints[_timestamp] = _total_staked_on_timestamp
+        return _total_staked_on_timestamp
