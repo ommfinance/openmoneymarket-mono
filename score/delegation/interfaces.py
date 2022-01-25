22cb0223995a745874c0c95a6eafff2cb46386d9
izyak <lbackup887@gmail.com>
2022-01-25 13:41:31 +0545

    Add interfaces and TypedDict

--- score/delegation/interfaces.py
+++ score/delegation/interfaces.py
@@ -11,6 +11,17 @@ class PrepDelegations(TypedDict):
     _votes_in_per: int
 
 
+class PrepICXDelegations(TypedDict):
+    _address: Address
+    _votes_in_per: int
+    _votes_in_icx: int
+
+
+class TotalStaked(TypedDict):
+    decimals: int
+    totalStaked: int
+
+
 class SystemInterface(InterfaceScore):
     @interface
     def getPRep(self, address: Address) -> dict:
@@ -22,10 +33,26 @@ class OmmTokenInterface(InterfaceScore):
     def details_balanceOf(self, _owner: Address) -> dict:
         pass
 
+    @interface
+    def getTotalStaked(self) -> TotalStaked:
+        pass
+
+
+class TokenInterface(InterfaceScore):
+    @interface
+    def balanceOf(self, _owner: int) -> int:
+        pass
+
 
 class LendingPoolCoreInterface(InterfaceScore):
     @interface
     def updatePrepDelegations(self, _delegations: List[PrepDelegations]):
+        pass
+
+
+class StakingInterface(InterfaceScore):
+    @interface
+    def getTodayRate(self) -> int:
         pass
 
 
